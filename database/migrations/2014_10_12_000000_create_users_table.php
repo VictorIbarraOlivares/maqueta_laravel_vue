b3cfd27561ce9bde78635be5ca4eb01b8550bf44
VictorIbarraOlivares <victor.ibarra.olivares@gmail.com>
2020-08-11 21:20:09 -0400

    se implementa migracion para user

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -1,8 +1,9 @@
 <?php
 
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
+use App\User;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
 
 class CreateUsersTable extends Migration
 {
@@ -15,11 +16,16 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('name');
+            $table->string('nombre');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->integer('rut')->nullable();
+            $table->integer('digito_verificador')->nullable();
             $table->rememberToken();
+            $table->string('verificado')->default( User::USUARIO_NO_VERIFICADO );
+            $table->string('token_verificacion')->nullable();
+            $table->string('admin')->default( User::USUARIO_REPRESENTANTE );
             $table->timestamps();
         });
     }
