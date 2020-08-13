790d9941dd6bd512254a3649632123ae7f7da40f
VictorIbarraOlivares <victor.ibarra.olivares@gmail.com>
2020-08-13 01:39:06 -0400

    arreglos de factory y seeders

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,5 +12,58 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+
+        /**
+         * Deben ser Seeds
+         */
+        // $factory->define(TipoDocumento::class, function (Faker $faker) {
+        //     return [
+        //         'nombre' => $faker->randomElement(['cuenta corriente', 'cuenta rut', 'cuenta vista']),
+        //     ];
+        // });
+
+        // $factory->define(TipoMedioPago::class, function (Faker $faker) {
+        //     return [
+        //         'nombre' => $faker->word,
+        //     ];
+        // });
+
+        // $factory->define(TipoCuenta::class, function (Faker $faker) {
+        //     return [
+        //         'nombre' => $faker->randomElement(['cuenta corriente', 'cuenta rut', 'cuenta vista']),
+        //     ];
+        // });
+
+        // $factory->define(Banco::class, function (Faker $faker) {
+        //     return [
+        //         'nombre' => $faker->randomElement(['banco bci', 'banco de chile', 'scotiabank', 'santander', 'banco falabella']),
+        //     ];
+        // });
+
+        // $factory->define(EntidadFinanciera::class, function (Faker $faker) {
+        //     return [
+        //         'nombre' => $faker->randomElement(['banco bci', 'banco de chile', 'scotiabank', 'santander', 'banco falabella']),
+        //     ];
+        // });
+
+        // $factory->define(MedioPago::class, function (Faker $faker) {
+        //     return [
+        //         'nombre' => $faker->randomElement(['banco bci', 'banco de chile', 'scotiabank', 'santander', 'banco falabella']),
+        //     ];
+        // });
+
+        // $factory->define(CentroSalud::class, function (Faker $faker) {
+        //     return [
+        //         'establecimiento' => $faker->word,
+        //         'comuna' => $faker->word,
+        //         'unidad_dependencia' => $faker->word,
+        //     ];
+        // });
+
+        // $factory->define(ResponsableRecepcion::class, function (Faker $faker) {
+        //     return [
+        //         'nombre' => $faker->word,
+        //     ];
+        // });
     }
 }
