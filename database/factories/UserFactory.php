4253cd091b19db854791da1049437f92130d9b45
VictorIbarraOlivares <victor.ibarra.olivares@gmail.com>
2020-08-18 20:47:11 -0400

    se terminan los factories. se crea carpeta para guardar los archivos de los documentos

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -3,13 +3,15 @@
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 
 use App\User;
-
+use App\Archivo;
 use App\Proveedor;
 use App\Documento;
 use App\MedioPago;
 use App\TipoCuenta;
 use App\OrdenCompra;
+use App\CentroSalud;
 use App\TipoMedioPago;
+use App\TipoDocumento;
 use App\EntidadFinanciera;
 
 use Faker\Generator as Faker;
@@ -53,10 +55,10 @@ $factory->define(Proveedor::class, function (Faker $faker) {
 });
 
 $factory->define(OrdenCompra::class, function (Faker $faker) {
-    $monto = $faker->numberBetween(1, 10000000);
+    $monto = $faker->numberBetween(1000, 10000000);
     return [
         'proveedor_id' => Proveedor::all()->random()->id,
-        'numero' => $faker->word, // Arreglar formato
+        'numero' => '1641-'. $faker->numberBetween(600, 2000) .'-SE20', 
         'monto' => $monto,
         'saldo' => $monto
         // 'estado' Arreglar en el documento factory
@@ -65,15 +67,55 @@ $factory->define(OrdenCompra::class, function (Faker $faker) {
 
 $factory->define(Documento::class, function (Faker $faker) {
     /**
-     * Obtener proveedor con al menos una orden de compra
+     * Obtener proveedor con al menos una orden de compra con estado disponible
      */
+    $proveedor = Proveedor::whereHas('ordenes_compra', function ($query){
+        $query->where('estado', OrdenCompra::OC_DISPONIBLE);
+    })->radmon();
+
+    $oc = OrdenCompra::where('proveedor_id', $proveedor->id)
+                     ->where('estado', OrdenCompra::OC_DISPONIBLE)
+                     ->random();
+
+    $consumeTotalOc = $faker->randomElement(['SI', 'NO']);
+    $montoDocumento = 0;
+    if ( $consumeTotalOc == 'SI' ) {
+        $montoDocumento = $oc->saldo;
+        $oc->saldo = 0;
+        $oc->estado = OrdenCompra::OC_NO_DISPONIBLE;
+        $oc->save();
+    } else {
+        $montoDocumento = $faker->numberBetween(1, $oc->saldo);
+        $oc->saldo -= $montoDocumento;
+        $oc->estado = $oc->saldo == 0 ? OrdenCompra::OC_NO_DISPONIBLE : OrdenCompra::OC_DISPONIBLE;
+        $oc->save();
+    }
+
+    $centroSalud = CentroSalud::all()->random();
+    $responsableRecepcion = ResponsableRecepcion::where('centro_salud_id', $centroSalud->id)->random();
+
+    $fechaActual = date('Y-m-d');
     return [
-        'nombre' => $faker->word,
+        'proveedor_id' => $proveedor->id,
+        'orden_compra_id' => $oc->id,
+        'tipo_documento_id' => TipoDocumento::all()->random()->id,
+        'centro_salud_id' => $centroSalud->id,
+        'responsable_recepcion_id' => $responsableRecepcion->id,
+        'fecha_documento' => date("Y-m-d", strtotime($fechaActual."- 1 week") ),
+        'numero_documento' => $faker->numberBetween(1000, 100000),
+        'monto_total' => $montoDocumento,
+        'fecha_recepcion' => date("Y-m-d", strtotime($fechaActual."- 3 days") ),
+        'observacion' => $faker->randomElement([ null, $faker->paragraph(1) ]),
     ];
 });
-/**
- * Faltan los archivos
- */
+
+$factory->define(Archivo::class, function (Faker $faker) {
+    return [
+        'documento_id' => Documento::doesntHave('archivos')->random()->id,
+        'url' => $faker->randomElement(['uno.pdf', 'dos.pdf', 'tres.pdf']),
+        'nombre' => $faker_word,
+    ];
+});
 
 
 
